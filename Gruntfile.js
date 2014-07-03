ca474ad5383fd7fe89734c93a99cfe19c7c9f974
Victor Queiroz <victorqueiroz@localhost.localdomain>
2014-07-03 18:42:20 -0300

    Gruntfile.js updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,15 +7,15 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: require('./package.json'),
 		bower: require('./bower.json'),
-    meta: {
-      banner: '/**\n' +
-      ' * <%= pkg.name %>\n' +
-      ' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      ' * @link <%= pkg.homepage %>\n' +
-      ' * @author <%= pkg.author.name %> (<%= pkg.author.email %>)\n' +
-      ' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
-      ' */\n'
-    }
+		meta: {
+			banner: '/**\n' +
+			' * <%= pkg.name %>\n' +
+			' * @version v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			' * @link <%= pkg.homepage %>\n' +
+			' * @author <%= pkg.author.name %> (<%= pkg.author.email %>)\n' +
+			' * @license MIT License, http://www.opensource.org/licenses/MIT\n' +
+			' */\n'
+		}
 	});
 
 };
